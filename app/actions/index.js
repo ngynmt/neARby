a477233fc9940d7586cd73580f45caafce2c074e
Michelle Nguyen <ngynmt@gmail.com>
2016-08-11 16:08:55 -0700

    feat/configure react native, redux

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,47 +1,43 @@
-import req from 'axios';
 export const PLACES_COLLECTION = 'PLACES_COLLECTION';
+export const SET_USER = 'SET_USER';
 
-export const fetchPlaces = function() {
-// const places = { body: {
-//     longitude: -122.0304880086256,
-//     latitude: 37.33240901400225,
-//     threejsLat: 0,
-//     threejsLon: 0
-//   }
-// };
+export const fetchPlaces = function(position) {
+    let collection = fetch('http://10.6.24.48:3000/location', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
 
-  // const collection = req.post('http://localhost:3000/location', places)
-    // .then((data) => {
-    //   console.log(data, 'DATA');
-    //   // return req.get('/location');
-    // })
-    // .catch((error) => {
-    //   console.log(error);
-    // });
-// const places = {
-//     longitude: -122.0304880086256,
-//     latitude: 37.33240901400225,
-//     threejsLat: 0,
-//     threejsLon: 0
-// };
-
-let collection = fetch('http://localhost:3000/location', {
-  method: 'POST',
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({
-    longitude: -122.0304880086256,
-    latitude: 37.33240901400225,
-    threejsLat: 0,
-    threejsLon: 0
+    body: JSON.stringify(position)
   })
-}).then((data) => console.log('DATA: ', data)).catch((err) => console.log('ERROR: ', err));
-console.log(collection, 'collection');
+  .then(function(response) {
+    if (response.status === 200) {
+      console.log(response);
+      response.json();
+    } else  {
+      console.log('error');
+    }
+  })
+  .catch(function(error) {
+    console.error(error);
+  });
 
+  console.log(collection, 'collection');
   return {
     type: PLACES_COLLECTION,
     payload: collection
+  };
+};
+
+export const setUser = function(name, picture) {
+  let user = {
+    name: name,
+    picture: picture
+  };
+
+  return {
+    type: SET_USER,
+    payload: user
   };
 };

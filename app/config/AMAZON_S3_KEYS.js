e0a01e651fb9af3cba1d1519ea33a6799cfbb2d5
Michelle Nguyen <ngynmt@gmail.com>
2016-08-25 21:02:50 -0700

    fixed untracked files

--- app/config/AMAZON_S3_KEYS.js
+++
@@ -1,8 +0,0 @@
-const AMAZON_S3 = {
-  bucket: 'nearbyapp',
-  region: 'us-west-1',
-  accessKey: 'AKIAINLG4742VEQQJGTQ',
-  secretKey: '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'
-};
-
-export default AMAZON_S3;

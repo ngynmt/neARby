44c4f73a4f5c2ac552bb359ac95c421b03756f6d
Michelle Nguyen <ngynmt@gmail.com>
2016-08-25 19:58:25 -0700

    config file

---
+++ app/config/AMAZON_S3_KEYS.js
@@ -0,0 +1,8 @@
+const AMAZON_S3 = {
+  bucket: 'nearbyapp',
+  region: 'us-west-1',
+  accessKey: 'AKIAINLG4742VEQQJGTQ',
+  secretKey: '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'
+};
+
+export default AMAZON_S3;
